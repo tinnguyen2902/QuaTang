ea09f0ebb0f43fd7737bd51db44063203f13707d
tinnguyen <tinnguyen2922000@gmail.com>
2026-06-27 12:43:29 +0900

    1

--- src/main/java/com/example/quatang/Controller/HomeController.java
+++ src/main/java/com/example/quatang/Controller/HomeController.java
@@ -1,5 +1,24 @@
-package com.example.quatang.Controller;public class HomeController {
-    public static void main(String[] args) {
-        
+package com.example.quatang.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("home")
+    public String home(){
+        return "home";
+    }
+    @GetMapping("/international")
+    public String internationalPage() {
+        return "international"; // Trả về file international.html trong templates
+    }
+    @GetMapping("/local")
+    public String localPage() {
+        return "local"; // Trả về file local.html trong templates khi click vào banner địa phương
+    }
+    @GetMapping("/domestic")
+    public String domesticPage() {
+        return "domestic"; // Trả về file domestic.html trong thư mục templates
     }
 }
